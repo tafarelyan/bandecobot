6f5d9530b74722fb83821a116a4ecb5bcd9e8477
Tafarel Yan <tafarel.yan@gmail.com>
2017-02-05 10:15:23 -0200

    First commit

---
+++ bot.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+from datetime import date
+
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, RegexHandler, ConversationHandler
+from selenium import webdriver
+
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+MENU = range(1)
+
+reply_keyboard = [['Fisica', 'Quimica'],
+                  ['Central', 'Prefeitura'],
+                  ['Done']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+driver = webdriver.PhantomJS()
+driver.set_window_size(1120, 550)
+
+u = Updater('321229309:AAF4uX6Zbmr1-0x6VbTScrjN6EA_xSvfPpg')
+dp = u.dispatcher
+
+
+def get_menu(text, day):
+    choices = {
+        'Central': '6',
+        'Prefeitura': '7',
+        'Fisica': '8',
+        'Quimica': '9'
+    }
+
+    website = 'https://uspdigital.usp.br/rucard/Jsp/cardapioSAS.jsp?codrtn='
+    driver.get(website + choices[text])
+    cardapio = driver.find_element_by_id('almoco' + day)
+    print(cardapio.text)
+    return cardapio.text
+
+
+def start(bot, update):
+    update.message.reply_text("Olá, aonde você quer comer hoje?", reply_markup=markup)
+
+    return MENU
+
+
+def menu(bot, update):
+    try:
+        weekdays = ['Segunda', 'Terca', 'Quarta', 'Quinta', 'Sexta']
+
+        # day = weekdays[date.today().weekday()]
+        day = weekdays[0]
+        menu = get_menu(update.message.text, day)
+        update.message.reply_text(menu)
+
+        update.message.reply_text("Quer ver o cardápio de outro lugar?", reply_markup=markup)
+
+        return MENU
+
+    except IndexError:
+        update.message.reply_text('O bandeco não abre hoje')
+
+        return ConversationHandler.END
+
+
+def done(bot, update):
+    update.message.reply_text("Muito obrigado, até a próxima!")
+
+    return ConversationHandler.END
+
+
+def error(bot, update, error):
+    logger.warn('Update "{}" caused error "{}"'.format(update, error))
+
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+
+    states={
+        MENU: [RegexHandler('^(Fisica|Quimica|Central|Prefeitura)$', menu)],
+    },
+
+    fallbacks=[RegexHandler('^Done$', done)]
+)
+
+dp.add_handler(conv_handler)
+
+dp.add_error_handler(error)
+
+u.start_polling()
+u.idle()
